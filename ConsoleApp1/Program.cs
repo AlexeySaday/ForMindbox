d9682e58c30e557dca031fc089c6a011e68e0e13
Alex Saday <alexeykildyushev@yandex.ru>
2022-09-20 10:30:08 +0300

    main commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public delegate void Message();
+        static void Main(string[] args)
+        {
+            int i = 1;
+            
+            object obj = i;
+            object j = obj;
+            obj = 2;
+            j = 3;
+            ++i;
+            Console.WriteLine(i);
+            Console.WriteLine(obj);
+            Console.WriteLine(j);
+        }
+
+    }
+    
+}
